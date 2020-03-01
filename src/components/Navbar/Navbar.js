184bf5aae2fc1b87d94cb56eab78a578dc499af1
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-01 17:11:23 -0600

    navbar stylings, logo, individual pokemon card border

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Navbar.scss';
-
+import pokeball from 'imgs/pokeball/pokeball.png';
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <h1 className='navbar-item'>pokedex</h1>
+      <div className='navbar-left'>
+        <img className='navbar-pokeball' src={pokeball} alt='pokeball' />
+        <h1 className='navbar-item'>pokedex</h1>
+      </div>
       <div className='navbar-portfoliolink'>
         by Thomas Foydel 2020{' '}
         <a href='https://thomasfoydel.com'>back to my portfolio</a>
